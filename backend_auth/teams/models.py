ef42ed8e25e18396b5070eaa2912f8940058228c
root <root@Sarvesh-Pc.localdomain>
2023-01-09 02:34:56 +0530

    teams api's

--- backend_auth/teams/models.py
+++ backend_auth/teams/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
 # Create your models here.
+
+class teams(models.Model):
+    # id = models.IntegerField(primary_key=True,null=False)
+    uid = models.CharField(max_length=50)
+    name = models.CharField(max_length=200,default='team name')
+    members  = models.ManyToManyField('User',on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_on =models.DateField(default=datetime.date.today)
+    
+    def __init__(self, *args):
+        super(teams, self).__init__(*args)
+        return self.name
--- backend_auth/teams/views.py
+++ backend_auth/teams/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .models import *
+import uuid
+from django.http import JsonResponse
 
 # Create your views here.
+
+def create_team(request):
+    try:
+        title = request.data['title']
+        namespace = uuid.UUID('12345678-1234-1234-1234-123456789012')
+        name = title
+        uid = uuid.uuid5(namespace, name)
+
+        teams.objects.create(
+            name = title,
+            uid = uid
+        )
+        
+        return JsonResponse({
+            'status':200,
+            'msg':'created team'
+        })
+    except Exception as e:
+        return JsonResponse({
+            'status':400,
+            'msg':str(e)
+        })
+        
+def add_member_to_team(request):
+    try:
+        user = request.data['userid']
+        team = request.data['uid']
+        
+        user = User.objects.get(username = user)
+        team = User.objects.get(uid = team)
+        team.members.add(user)
+        team.save()
+        return JsonResponse({
+            'status':200,
+            'msg':'added member to team'
+        })
+    except Exception as e:
+        return JsonResponse({
+            'status':400,
+            'msg':str(e)
+        })
+        
