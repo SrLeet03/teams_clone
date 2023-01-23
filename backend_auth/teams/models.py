3abf081e60c96a2e5856ee5c4fba02d6b981aae4
Shreyansh <shreyanshsxn18@gmail.com>
2023-01-23 19:19:36 +0530

    added teams model and views

--- backend_auth/teams/models.py
+++ backend_auth/teams/models.py
@@ -3,14 +3,33 @@ from django.contrib.auth.models import User
 import datetime
 # Create your models here.
 
-class teams(models.Model):
+class Team(models.Model):
     # id = models.IntegerField(primary_key=True,null=False)
     uid = models.CharField(max_length=50)
     name = models.CharField(max_length=200,default='team name')
-    members  = models.ManyToManyField('User',on_delete=models.SET_NULL)
+    members  = models.ManyToManyField(User, related_name='teams_members')
     created_by = models.ForeignKey(User,on_delete=models.CASCADE)
     created_on =models.DateField(default=datetime.date.today)
     
-    def __init__(self, *args):
-        super(teams, self).__init__(*args)
+    def __str__(self):
         return self.name
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='channels')
+
+    def __str__(self):
+        return self.title
+
+class Message(models.Model):
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.text[:20] + '...'
+
--- backend_auth/teams/views.py
+++ backend_auth/teams/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from .models import *
+from rest_framework.decorators import api_view
 import uuid
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
+@csrf_exempt
+@api_view(["POST"])
 def create_team(request):
     try:
         title = request.data['title']
         namespace = uuid.UUID('12345678-1234-1234-1234-123456789012')
         name = title
         uid = uuid.uuid5(namespace, name)
-
-        teams.objects.create(
+        user_id = request.data['username']
+        user = User.objects.get(username = user_id)
+        Team.objects.create(
             name = title,
-            uid = uid
+            uid = uid,
+            created_by = user
         )
         
         return JsonResponse({
